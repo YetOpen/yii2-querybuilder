83aebb6432aa221007ef82c54750a31ebcedb775
Marco Piazza <marco.piazza@yetopen.com>
2023-12-28 17:30:26 +0100

    Loading localization file for query builder.

--- src/QueryBuilderAsset.php
+++ src/QueryBuilderAsset.php
@@ -14,7 +14,7 @@ class QueryBuilderAsset extends AssetBundle {
     public $sourcePath = '@bower/jquery-querybuilder/dist';
 
     public $js = [
-        'js/query-builder.standalone.min.js',
+        YII_DEBUG ? 'js/query-builder.standalone.js' : 'js/query-builder.standalone.min.js',
     ];
 
     public $css = [
@@ -24,5 +24,17 @@ class QueryBuilderAsset extends AssetBundle {
     public $depends = [
         'yii\web\JqueryAsset'
     ];
+
+    public function init()
+    {
+        parent::init();
+        $language = \Yii::$app->language;
+        // Check if the language file exists, if not use the abbreviated version, if it exists
+        if (file_exists(\Yii::getAlias($this->sourcePath . "/i18n/query-builder.{$language}.js"))) {
+            $this->js[] = "i18n/query-builder.{$language}.js";
+        } elseif (file_exists(\Yii::getAlias($this->sourcePath . "/i18n/query-builder.{$language}.min.js"))) {
+            $this->js[] = "i18n/query-builder.{$language}.min.js";
+        }
+    }
 
 }
